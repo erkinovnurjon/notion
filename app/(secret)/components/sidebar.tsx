9d7ffcfd0e0afcd0193ea3cff79de5a71f9503b3
Nurjon <nurjon2477@gmail.com>
2024-01-31 15:27:06 +0500

    stripe

--- app/(secret)/components/sidebar.tsx
+++ app/(secret)/components/sidebar.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { cn } from "@/lib/utils";
@@ -15,7 +14,7 @@ import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { DocumentList } from "./document-list";
 import { Item } from "./item";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { UserBox } from "./user-box";
 import { Progress } from "@/components/ui/progress";
@@ -30,6 +29,9 @@ import { toast } from "sonner";
 import { Navbar } from "./navbar";
 import { useSearch } from "@/hooks/use-search";
 import { useSettings } from "@/hooks/use-settings";
+import useSubscription from "@/hooks/use-subscription";
+import { useUser } from "@clerk/clerk-react";
+import { Loader } from "@/components/ui/loader";
 
 export const Sidebar = () => {
   const isMobile = useMediaQuery("(max-width: 770px)");
@@ -37,6 +39,7 @@ export const Sidebar = () => {
   const params = useParams();
   const search = useSearch();
   const settings = useSettings();
+  const { user } = useUser();
 
   const createDocument = useMutation(api.document.createDocument);
 
@@ -47,12 +50,20 @@ export const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
   const [isResetting, setIsResetting] = useState(false);
 
+  const { isLoading, plan } = useSubscription(
+    user?.emailAddresses[0]?.emailAddress!
+  );
+
+  const documents = useQuery(api.document.getAllDocuments);
+
   useEffect(() => {
     if (isMobile) {
       collapse();
     } else {
       reset();
     }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMobile]);
 
   const collapse = () => {
@@ -112,6 +123,11 @@ export const Sidebar = () => {
   };
 
   const onCreateDocument = () => {
+    if (documents?.length && documents.length >= 3 && plan === "Free") {
+      toast.error("You can only create 3 documents in the free plan");
+      return;
+    }
+
     const promise = createDocument({
       title: "Untitled",
     }).then((docId) => router.push(`/documents/${docId}`));
@@ -129,7 +145,7 @@ export const Sidebar = () => {
     <>
       <div
         className={cn(
-          "group/sidebar h-screen bg-secondary overflow-y-auto  flex w-60 flex-col z-50 sticky left-0 top-0",
+          "group/sidebar h-screen bg-secondary overflow-y-auto flex w-60 flex-col z-50 sticky left-0 top-0",
           isResetting && "transition-all ease-in duration-300",
           isMobile && "w-0"
         )}
@@ -186,18 +202,39 @@ export const Sidebar = () => {
         />
 
         <div className="absolute bottom-0 px-2 bg-white/50 dark:bg-black/50 py-4 w-full">
-          <div className="flex justify-between items-center">
-            <div className="flex items-center gap-1 text-[13px]">
-              <Rocket />
-              <p className="opacity-70 font-bold">Free plan</p>
+          {isLoading ? (
+            <div className="w-full flex justify-center items-center">
+              <Loader />
             </div>
-            <p className="text-[13px] opacity-70">{arr.length}/3</p>
-          </div>
-
-          <Progress
-            value={arr.length >= 3 ? 100 : arr.length * 33.33}
-            className="mt-2"
-          />
+          ) : (
+            <>
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-1 text-[13px]">
+                  <Rocket />
+                  <p className="opacity-70 font-bold">{plan} plan</p>
+                </div>
+                {plan === "Free" ? (
+                  <p className="text-[13px] opacity-70">
+                    {documents?.length}/3
+                  </p>
+                ) : (
+                  <p className="text-[13px] opacity-70">
+                    {documents?.length} notes
+                  </p>
+                )}
+              </div>
+              {plan === "Free" && (
+                <Progress
+                  value={
+                    documents?.length && documents.length >= 3
+                      ? 100
+                      : (documents?.length || 0) * 33.33
+                  }
+                  className="mt-2"
+                />
+              )}
+            </>
+          )}
         </div>
       </div>
 
